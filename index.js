9fc7b0fffe3137ae6d2e889fe9d99dd1d768d700
Chirag Bhardwaj <developerchiragb@gmail.com>
2020-11-30 23:54:08 +0530

    set up the assets folder

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(expressLayouts);
 app.use('/',indexRouter);
 app.set('view engine','ejs');
 app.set('views','./views');
+app.use(express.static('./assets'));
 app.listen(port,(err)=>{
     if(err)
     {
