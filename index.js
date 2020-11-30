d55a0be957f70a05c419ebd023a31faf431c0651
Chirag Bhardwaj <developerchiragb@gmail.com>
2020-12-01 00:02:54 +0530

    setup individual style for individual pages using layout extractStyle and extractScript

--- index.js
+++ index.js
@@ -3,15 +3,26 @@ const express = require('express');
 const app = express();
 
 const port = 8080;
+
+//express-ejs-layouts
 const expressLayouts = require('express-ejs-layouts');
 
 // use express router
 const indexRouter = require('./routes/index');
+
+
 app.use(expressLayouts);
+
+//extract style and scripts from sub pages into the layout 
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
 app.use('/',indexRouter);
 app.set('view engine','ejs');
 app.set('views','./views');
 app.use(express.static('./assets'));
+
+
 app.listen(port,(err)=>{
     if(err)
     {
