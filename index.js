b99cf760dc34c763054b3749573a3de54b945e24
Chirag Bhardwaj <developerchiragb@gmail.com>
2020-11-30 18:10:57 +0530

    added git and npm start

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const port = 8000;
+
+app.listen(port,(err)=>{
+    if(err)
+    {
+        console.log(`Error in running express server: ${err}`);
+        return;
+    }
+    console.log(`Server is up and running on port: ${port}`);
+
+})
+
