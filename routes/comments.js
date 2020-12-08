597d98c10c13dde26b0813ee475102e097784cfb
developerchiragb <developerchiragb@gmail.com>
2020-12-09 00:41:56 +0530

    updating a user's profile

--- routes/comments.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const commentController = require('../controllers/comments_controller');
-const router = express.Router();
-const passport = require('passport');
-router.post('/create',passport.checkAuthentication,commentController.create);
-
-module.exports = router;
--- routes/posts.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const passport = require('passport')
-const postsController = require('../controllers/posts_controller');
-
-router.post('/create',passport.checkAuthentication,postsController.create);
-
-router.post('/add-comment',passport.checkAuthentication,postsController.addComment);
-module.exports = router;
